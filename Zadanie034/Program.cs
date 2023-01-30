765a7f0dddd93083b9b92623f3022dbf55eaa3fd
KMikhail <fin2fix@gmail.com>
2023-01-30 17:52:35 +0000

    Add Zadanie036

---
+++ Zadanie034/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int[] GetArray(int dimension)
+{
+  int[] arr = new int[dimension];
+  Random rnd = new Random();
+
+  for (int i = 0; i < dimension; i++)
+  {
+    arr[i] = rnd.Next(100, 1000);
+  }
+  return arr;
+}
+
+int FindNumberEven(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] % 2 == 0) count = count + 1;
+  }
+  return count;
+}
+
+int getSizeArray(string message)
+{
+  int result = 0;
+  while (true)
+  {
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+    { break; }
+    else
+    {
+      Console.WriteLine("Введено неверное число!");
+      Console.WriteLine();
+    }
+  }
+  return result;
+}
+
+int size = getSizeArray("Введите размер массива (количество элементов массива)");
+int[] array = GetArray(size);
+Console.WriteLine($"Массив {string.Join(", ", array)} ");
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве = {FindNumberEven(array)} ");
+
---
+++ Zadanie036/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] GetArray(int dimension)
+{
+  int[] arr = new int[dimension];
+  Random rnd = new Random();
+
+  for (int i = 0; i < dimension; i++)
+  {
+    arr[i] = rnd.Next(-1000, 1001);
+  }
+  return arr;
+}
+
+
+int getSizeArray(string message)
+{
+  int result = 0;
+  while (true)
+  {
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+    { break; }
+    else
+    {
+      Console.WriteLine("Введено неверное число!");
+      Console.WriteLine();
+    }
+  }
+  return result;
+}
+
+int FindSumOddPosition(int[] arr)
+{
+  int sum = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i % 2 == 1) sum = sum + arr[i];
+  }
+  return sum;
+}
+
+int size = getSizeArray("Введите размер массива (количество элементов массива)");
+int[] array = GetArray(size);
+Console.WriteLine($"Массив {string.Join(", ", array)} ");
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на нечетной позиции массива = {FindSumOddPosition(array)} ");
